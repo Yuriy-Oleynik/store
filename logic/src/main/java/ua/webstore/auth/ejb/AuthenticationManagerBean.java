e34d311bdf70c0b63eb6f6ab36e6794b508e980f
Юрий <yurikua11@gmail.com>
2018-08-23 17:58:29 +0300

    add auth logic

---
+++ logic/src/main/java/ua/webstore/auth/ejb/AuthenticationManagerBean.java
@@ -0,0 +1,41 @@
+package ua.webstore.auth.ejb;
+
+import org.apache.commons.lang3.StringUtils;
+import ua.webstore.auth.domain.Credentials;
+import ua.webstore.auth.domain.User;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+@LocalBean
+public class AuthenticationManagerBean {
+
+    @PersistenceContext(unitName = "examplePU")
+    private EntityManager entityManager;
+
+    public boolean loginAsUser(String email, String password) {
+        if (StringUtils.isEmpty(email) || StringUtils.isEmpty(password)) {
+            return false;
+        }
+
+        Credentials credentials = entityManager.find(Credentials.class, email);
+        if (credentials == null) {
+            return false;
+        }
+
+        if(!password.equals(credentials.getPassword())){
+            return false;
+        }
+
+        User user = credentials.getUser();
+        if(user == null){
+            return false;
+        }
+
+        return true;
+    }
+
+}
